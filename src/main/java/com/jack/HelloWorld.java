e8f676b00c78041aefc111f400c1a04e688bfc63
young <2354902964@qq.com>
2024-09-25 13:34:44 +0800

    1

---
+++ src/main/java/com/jack/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.jack;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 1.创建HelloWorldMainApplication类,并声明这是一个主程序类也是个SpringBoot应用
+ */
+@SpringBootApplication
+public class HelloWorld {
+    /**
+     * 2.编写main方法
+     */
+    public static void main(String[] args) {
+        //3.开始启动主程序类
+        SpringApplication.run(HelloWorld.class,args);
+    }
+}
